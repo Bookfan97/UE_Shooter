51050a20aa692019757eb878fc2efe2fb9bf8694
Noah Day <sheltielover432@gmail.com>
2020-07-24 19:50:29 -0400

    Death Animations

--- Source/SimpleShooter/ShooterCharacter.h
+++ Source/SimpleShooter/ShooterCharacter.h
@@ -15,14 +15,13 @@ class SIMPLESHOOTER_API AShooterCharacter : public ACharacter
 public:
 	// Sets default values for this character's properties
 	AShooterCharacter();
-
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
+	UFUNCTION(BlueprintPure)
+	bool IsDead() const;
 
 protected:
 	// Called when the game starts or when spawned
